3ba1a300bfbffe3ac32f4df5960ffacbd97b044c
Jan Thiel <JanThiel@users.noreply.github.com>
2022-01-31 17:10:15 +0100

    1.3.1 update merge (#7)
    * - Register features
    - Enable/disable features
    - Check for enabled single/group features
    - Manage registered features (get all features / update features)
    
    * - Set registered features to global var.
    - network_id => blog_id
    - switch to blog to save/get blog_features into/from blog options
    - Put features-check logic into function
    
    * WP-Cli support
    
    * Update Readme to reflect reality
    
    * Text update
    
    * Plugin Header fix
    
    * Plugin Header FIx
    
    * Fix: Get all features / Save features (#6)
    
    * Release 1.1 (#5)
    
    * - Register features
    - Enable/disable features
    - Check for enabled single/group features
    - Manage registered features (get all features / update features)
    
    * - Set registered features to global var.
    - network_id => blog_id
    - switch to blog to save/get blog_features into/from blog options
    - Put features-check logic into function
    
    * WP-Cli support
    
    * Update Readme to reflect reality
    
    * Text update
    
    * Plugin Header fix
    
    * Plugin Header FIx
    
    Co-authored-by: André Schneider <24269043+WDD-AS@users.noreply.github.com>
    
    * Fix: Get all features via (global) options
    Fix: Update (global) options
    
    * Sorting all features
    
    * Add success return parameter
    
    * - Store the "enabled"-status field as an array-field to ensure the main-site status
    - Adjust logic to get the changed "enabled"-status field
    
    * Update wp-featurizer.php
    
    Co-authored-by: Jan Thiel <JanThiel@users.noreply.github.com>
    
    * Code cleanup. Better data separation.
    
    * Optimize handling of unchanged features
    
    Co-authored-by: André Schneider <24269043+WDD-AS@users.noreply.github.com>

--- wp-featurizer.php
+++ wp-featurizer.php
@@ -1,7 +1,8 @@
 <?php
 /**
  * WP-Featurizer (F8R)
- * MU-Plugin
+ *
+ * THIS PLUGIN HAS TO BE INSTALLED AS MU-PLUGIN
  *
  * @package           Hive-IT-GmbH/wp-featurizer
  * @author            Hive-IT GmbH
@@ -9,14 +10,14 @@
  * @license           GPL-3.0-or-later
  *
  * @wordpress-plugin
- * Plugin Name:	WP-Featurizer (F8R)
- * Plugin URI: 	https://github.com/Hive-IT-GmbH/wp-featurizer
- * Description: This plugin allows you to use Feature Flags in Plugins and Themes. This plugin requires a WP-Multisite and control over the code and options.
- * Version: 	1.2.1
- * Author: 	Hive-IT-GmbH
- * Author URI: 	https://hive-it.de/
- * License: 	GPLv3 or later
- * Network: 	true
+ * Plugin Name:   WP-Featurizer (F8R)
+ * Plugin URI:    https://github.com/Hive-IT-GmbH/wp-featurizer
+ * Description:   This plugin allows you to use Feature Flags in Plugins and Themes. This plugin requires a WP-Multisite and control over the code and options.
+ * Version:       1.3.1
+ * Author:        Hive-IT-GmbH
+ * Author URI:    https://hive-it.de/
+ * License:       GPLv3 or later
+ * Network:       true
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -49,8 +50,10 @@ function f8r_register_feature( string $vendor, string $group, string $feature ) 
  * @param string $group
  * @param string $feature
  * @param int $blog_id
+ *
+ * @return bool
  */
-function f8r_enable_feature( string $vendor, string $group, string $feature = '', int $blog_id = 0 ) {
+function f8r_enable_feature( string $vendor, string $group, string $feature = '', int $blog_id = 0 ): bool {
 
 	global $f8r_registered_features;
 
@@ -68,22 +71,30 @@ function f8r_enable_feature( string $vendor, string $group, string $feature = ''
 		switch_to_blog( $blog_id );
 	}
 
+	// Already enabled
+	if ( f8r_is_feature_enabled( $vendor, $group, $feature ) ) {
+		return true;
+	}
+
 	$blog_features = get_option( 'f8r_features', array() );
 
 	if ( '' !== $feature ) {
 		// enable single feature
-		$blog_features[ $vendor ][ $group ][ $feature ] = true;
+		$blog_features[ $vendor ][ $group ][ $feature ]['enabled'] = true;
 	} else {
 		// enable all group-features
 		foreach ( $f8r_registered_features[ $vendor ][ $group ] as $group_feature => $value ) {
-			$blog_features[ $vendor ][ $group ][ $group_feature ] = true;
+			$blog_features[ $vendor ][ $group ][ $group_feature ]['enabled'] = true;
 		}
 	}
-	update_option( 'f8r_features', $blog_features );
+	$updated = update_option( 'f8r_features', $blog_features );
 
 	if ( $blog_id != 0 ) {
 		restore_current_blog();
 	}
+
+	return $updated;
+  
 }
 
 /**
@@ -93,8 +104,11 @@ function f8r_enable_feature( string $vendor, string $group, string $feature = ''
  * @param string $group
  * @param string $feature
  * @param int $blog_id
+ *
+ * @return bool
  */
-function f8r_disable_feature( string $vendor, string $group, string $feature = '', int $blog_id = 0 ) {
+function f8r_disable_feature( string $vendor, string $group, string $feature = '', int $blog_id = 0 ): bool {
+	global $f8r_registered_features;
 
 	$vendor  = sanitize_key( $vendor );
 	$group   = sanitize_key( $group );
@@ -110,14 +124,21 @@ function f8r_disable_feature( string $vendor, string $group, string $feature = '
 		switch_to_blog( $blog_id );
 	}
 
+	// Already disabled
+	if ( ! f8r_is_feature_enabled( $vendor, $group, $feature ) ) {
+		return true;
+	}
+
 	$blog_features = get_option( 'f8r_features', array() );
 
-	if ( '' !== $feature ) {
-		// disable/remove single feature
-		unset( $blog_features[ $vendor ][ $group ][ $feature ] );
-	} else {
-		// disable/remove all group-features
-		unset( $blog_features[ $vendor ][ $group ] );
+	// disable/remove all group-features
+	foreach ( $f8r_registered_features[ $vendor ][ $group ] as $group_feature => $feature_data ) {
+		if ( ( $group_feature == $feature && '' !== $feature ) || '' === $feature ) {
+			unset( $blog_features[ $vendor ][ $group ][ $group_feature ]['enabled'] );
+		}
+		if ( empty( $blog_features[ $vendor ][ $group ][ $group_feature ] ) ) {
+			unset( $blog_features[ $vendor ][ $group ][ $group_feature ] );
+		}
 	}
 
 	// remove empty data
@@ -128,16 +149,19 @@ function f8r_disable_feature( string $vendor, string $group, string $feature = '
 	if ( empty( $blog_features[ $vendor ] ) ) {
 		unset( $blog_features[ $vendor ] );
 	}
-
+	$updated = false;
 	if ( empty( $blog_features ) ) {
-		delete_option( 'f8r_features' );
+		$updated = delete_option( 'f8r_features' );
 	} else {
-		update_option( 'f8r_features', $blog_features );
+		$updated = update_option( 'f8r_features', $blog_features );
 	}
 
 	if ( $blog_id != 0 ) {
 		restore_current_blog();
 	}
+
+	return $updated;
+
 }
 
 /**
@@ -150,15 +174,24 @@ function f8r_disable_feature( string $vendor, string $group, string $feature = '
  *
  * @return bool
  */
-function f8r_is_feature_enabled( string $vendor, string $group, string $feature = '', int $blog_id = 0 ) {
+function f8r_is_feature_enabled( string $vendor, string $group, string $feature = '', int $blog_id = 0 ): bool {
 
 	global $f8r_registered_features;
 
 	$vendor     = sanitize_key( $vendor );
 	$group      = sanitize_key( $group );
 	$feature    = sanitize_key( $feature );
-	$blog_id = absint( $blog_id );		
+	$blog_id    = absint( $blog_id );
 	$is_enabled = true;
+
+	// Shortcircuit enabled check
+	/**
+	 * Filter
+	 */
+	$enabled_filter = apply_filters('f8r/is_feature_enabled', null, $vendor, $group, $feature);
+	if ( null !== $enabled_filter ) {
+		return $enabled_filter;
+	}
 
 	// Is group or feature registered?
 	if ( ! f8r_check_registered_features( 'f8r_is_feature_enabled', $vendor, $group, $feature ) ) {
@@ -173,7 +206,7 @@ function f8r_is_feature_enabled( string $vendor, string $group, string $feature 
 
 	if ( '' !== $feature ) {
 		// check against single feature
-		if ( ! isset( $blog_features[ $vendor ][ $group ][ $feature ] ) ) {
+		if ( ! ( $blog_features[ $vendor ][ $group ][ $feature ]['enabled'] ?? false ) ) {
 			$is_enabled = false;
 		}
 	} else {
@@ -184,7 +217,7 @@ function f8r_is_feature_enabled( string $vendor, string $group, string $feature 
 		// Are all feature enabled?
 		if ( $is_enabled ) {
 			foreach ( $f8r_registered_features[ $vendor ][ $group ] as $registered_feature => $value ) {
-				if ( ! isset( $blog_features[ $vendor ][ $group ][ $registered_feature ] ) ) {
+				if ( ! ( $blog_features[ $vendor ][ $group ][ $registered_feature ]['enabled'] ?? false ) ) {
 					$is_enabled = false;
 					break;
 				}
@@ -206,32 +239,51 @@ function f8r_is_feature_enabled( string $vendor, string $group, string $feature 
  *
  * @return array
  */
-function f8r_get_all_features( int $blog_id = 0 ) {
+function f8r_get_all_features( int $blog_id = 0 ): array {
 	global $f8r_registered_features;
 	
 	$all_features =	$f8r_registered_features;
+	$global_features_data = get_site_option( 'f8r_features', array() );
 
 	if ( $blog_id != 0 ) {
-		$all_features = get_network_option( null, 'f8r_features', array() );
 		switch_to_blog( $blog_id );
 	}
 	
 	$blog_features = get_option( 'f8r_features', array() );
 
 	if ( $all_features ) {
+		// sorting vendors
+		ksort( $all_features );
 		foreach ( $all_features as $vendor => $groups ) {
+			// sorting groups
+			ksort( $all_features[ $vendor ] );
 			foreach ( $groups as $group => $features ) {
+				// sorting features
+				ksort( $all_features[ $vendor ][ $group ] );
 				foreach ( $features as $feature => $enabled ) {
 					if ( isset( $blog_features[ $vendor ][ $group ] ) ) {
-						$all_features[ $vendor ][ $group ][ $feature ]['enabled'] = array_key_exists( $feature, $blog_features[ $vendor ][ $group ] );
+						$all_features[ $vendor ][ $group ][ $feature ]['enabled'] = $blog_features[ $vendor ][ $group ][ $feature ]['enabled'];
 					} else {
 						$all_features[ $vendor ][ $group ][ $feature ]['enabled'] = false;
 					}
+					// Add global feature data
+					$all_features[ $vendor ][ $group ][ $feature ]['teaser_title']     = '';
+					$all_features[ $vendor ][ $group ][ $feature ]['teaser_text_html'] = '';
+					$all_features[ $vendor ][ $group ][ $feature ]['teaser_url']       = '';
+					if ( isset( $global_features_data[ $vendor ][ $group ][ $feature ]['teaser_title'] ) ) {
+						$all_features[ $vendor ][ $group ][ $feature ]['teaser_title'] = $global_features_data[ $vendor ][ $group ][ $feature ]['teaser_title'];
+					}
+					if ( isset( $global_features_data[ $vendor ][ $group ][ $feature ]['teaser_text_html'] ) ) {
+						$all_features[ $vendor ][ $group ][ $feature ]['teaser_text_html'] = $global_features_data[ $vendor ][ $group ][ $feature ]['teaser_text_html'];
+					}
+					if ( isset( $global_features_data[ $vendor ][ $group ][ $feature ]['teaser_url'] ) ) {
+						$all_features[ $vendor ][ $group ][ $feature ]['teaser_url'] = $global_features_data[ $vendor ][ $group ][ $feature ]['teaser_url'];
+					}
+
 				}
 			}
 		}
 	}
-
 	if ( $blog_id != 0 ) {
 		restore_current_blog();
 	}
@@ -240,13 +292,13 @@ function f8r_get_all_features( int $blog_id = 0 ) {
 }
 
 /**
- * Update feature
+ * Update global feature data
  *
  * @param array $feature_data
  *
- * @return false|void
+ * @return bool
  */
-function f8r_update_feature( array $feature_data ) {
+function f8r_update_feature( array $feature_data ): bool {
 
 	global $f8r_registered_features;
 
@@ -265,8 +317,9 @@ function f8r_update_feature( array $feature_data ) {
 	$f8r_registered_features[ $vendor ][ $group ][ $feature ]['teaser_title']     = $teaser_title;
 	$f8r_registered_features[ $vendor ][ $group ][ $feature ]['teaser_text_html'] = $teaser_text_html;
 	$f8r_registered_features[ $vendor ][ $group ][ $feature ]['teaser_url']       = $teaser_url;
-	update_network_option( null, 'f8r_features', $f8r_registered_features );
+	update_site_option( 'f8r_features', $f8r_registered_features );
 
+	return true;
 }
 
 /**
@@ -280,7 +333,7 @@ function f8r_update_feature( array $feature_data ) {
  *
  * @return bool
  */
-function f8r_check_registered_features( string $function_name, string $vendor, string $group, string $feature = '', $feature_check = false ) {
+function f8r_check_registered_features( string $function_name, string $vendor, string $group, string $feature = '', $feature_check = false ): bool {
 	global $f8r_registered_features;
 
 	$function_name = sanitize_key( $function_name );
